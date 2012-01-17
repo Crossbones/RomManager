b4bd19dcfaa28503a259d5b53fca880bf1eab9fe
Mike Wielgosz <dev.mwielgosz@gmail.com>
2012-01-17 15:12:26 +0000

    RomManager: add manifest for currently available ROMs

---
+++ manifest.js
@@ -0,0 +1,104 @@
+{
+    "version": "1",
+    "homepage": "http://goo-inside.me/roms/crossbones",
+    "donate": "https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=EX9W3A7743TZS",
+    "roms": [
+        {
+            "device": "crespo",
+            "product": "Crossbones",
+            "name": "0.1.3",
+            "summary": "Crossbones ROM - ICS - 0.1.3\nReleased: 01/08/2012",
+            "modversion": "Crossbones-crespo-IML74K-0.1.3",
+            "url": "http://goo-inside.me/roms/crossbones/crespo/Crossbones-crespo-IML74K-0.1.3.zip"
+            "addons": [
+                {
+                    "name": "Google Apps",
+                    "url": "http://goo-inside.me/roms/crossbones/gapps/gapps-xb-122411-signed.zip"
+                }
+            ]
+        },
+        {
+            "product": "Crossbones",
+            "device": "crespo",
+            "name": "Crossbones 0.1.2",
+            "summary": "Crossbones ROM - ICS - 0.1.2\nReleased: 01/05/2012",
+            "modversion": "Crossbones-crespo-IML74K-0.1.2",
+            "url": "http://goo-inside.me/roms/crossbones/crespo/Crossbones-crespo-IML74K-0.1.2.zip"
+            "addons": [
+                {
+                    "name": "Google Apps",
+                    "url": "http://goo-inside.me/roms/crossbones/gapps/gapps-xb-122411-signed.zip"
+                }
+            ]
+        },
+        {
+            "device": "crespo4g",
+            "product": "Crossbones",
+            "name": "0.1.3",
+            "summary": "Crossbones ROM - ICS - 0.1.3\nReleased: 01/08/2012",
+            "modversion": "Crossbones-crespo4g-IML74K-0.1.3",
+            "url": "http://goo-inside.me/roms/crossbones/crespo4g/Crossbones-crespo4g-IML74K-0.1.3.zip"
+            "addons": [
+                {
+                    "name": "Google Apps",
+                    "url": "http://goo-inside.me/roms/crossbones/gapps/gapps-xb-122411-signed.zip"
+                }
+            ]
+        },
+        {
+            "device": "crespo4g",
+            "product": "Crossbones",
+            "name": "Crossbones 0.1.2",
+            "summary": "Crossbones ROM - ICS - 0.1.2\nReleased: 01/05/2012",
+            "modversion": "Crossbones-crespo4g-IML74K-0.1.2",
+            "url": "http://goo-inside.me/roms/crossbones/crespo4g/Crossbones-crespo4g-IML74K-0.1.2.zip"
+            "addons": [
+                {
+                    "name": "Google Apps",
+                    "url": "http://goo-inside.me/roms/crossbones/gapps/gapps-xb-122411-signed.zip"
+                }
+            ]
+        },
+        {
+            "device": "crespo4g",
+            "product": "Crossbones",
+            "name": "Crossbones 0.1.1",
+            "summary": "Crossbones ROM - ICS - 0.1.1\nReleased: 01/01/2012",
+            "modversion": "Crossbones-crespo4g-IML74K-0.1.1",
+            "url": "http://goo-inside.me/roms/crossbones/crespo4g/Crossbones-crespo4g-IML74K-0.1.1.zip"
+            "addons": [
+                {
+                    "name": "Google Apps",
+                    "url": "http://goo-inside.me/roms/crossbones/gapps/gapps-xb-122411-signed.zip"
+                }
+            ]
+        },
+        {
+            "device": "crespo4g",
+            "product": "Crossbones",
+            "name": "Crossbones 0.1.0",
+            "summary": "Crossbones ROM - ICS - 0.1.0\nReleased: 12/27/2011",
+            "modversion": "Crossbones-crespo4g-IML74K-0.1.0",
+            "url": "http://goo-inside.me/roms/crossbones/crespo4g/Crossbones-crespo4g-IML74K-0.1.0.zip"
+            "addons": [
+                {
+                    "name": "Google Apps",
+                    "url": "http://goo-inside.me/roms/crossbones/gapps/gapps-xb-122411-signed.zip"
+                }
+            ]
+        },
+        {
+            "device": "toro",
+            "product": "Crossbones",
+            "name": "0.1.3",
+            "summary": "Crossbones ROM - ICS - 0.1.3\nReleased: 01/08/2012",
+            "modversion": "Crossbones-toro-IML74K-0.1.3",
+            "url": "http://goo-inside.me/roms/crossbones/crespo/Crossbones-toro-IML74K-0.1.3.zip"
+            "addons": [
+                {
+                    "name": "Google Apps",
+                    "url": "http://goo-inside.me/roms/crossbones/gapps/gapps-xb-122411-signed.zip"
+                }
+            ]
+        }
+}
